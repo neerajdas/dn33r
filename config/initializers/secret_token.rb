2745eff228bd937147930b8d52301762691f4bd2
inkgab <keithar123@gmail.com>
2014-02-17 02:02:22 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dn33r::Application.config.secret_key_base = 'a92e1f2606831521ae9c0a6b5740e3823298d54377cdf572721b5f12e41a8a2d9e6ebae581e29b20bdb2b38b735f2b9e6114aefbbeb33c0517516791b3f30986'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dn33r::Application.config.session_store :cookie_store, key: '_dn33r_session'
